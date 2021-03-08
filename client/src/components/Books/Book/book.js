655640bd60c1c240eee6fb1c81476e2a479f26a9
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-08 18:27:40 +0530

    Add files via upload

---
+++ client/src/components/Books/Book/book.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'; 
+// import ThumbUpAltIcon from '@material-ui/icon/ThumbUpAl';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+// import { post } from '../../../../../server/routes/auth';
+
+// import useStyles from './styles';
+
+const Book = ({ book }) => {
+    // const classes = useStyles();
+    return(
+       <Card >
+           <CardMedia>{book.booktitle}</CardMedia>
+           <CardMedia src={book.selectedFile}/>
+           <div>
+               <Typography variant="h6">Posted By: {book.username}</Typography>
+               <Typography variant="body2">{moment(book.createdAt).fromNow()}</Typography>
+           </div>
+           <div>
+               <Button style={{color: 'white'}} size="small" onClick={()=>{}}>
+                   <MoreHorizIcon fontSize="default"/>
+               </Button>
+           </div>
+           {/* <div>
+                <Typography variant="body2" color="textSecondary">{book.tags.map((tag) => '#${tag} ')}</Typography>
+           </div> */}
+           <CardContent>
+                <Typography variant="h5" gutterBottom>Author: {book.author}</Typography>
+                <Typography variant="h5" gutterBottom>Publisher: {book.publisher}</Typography>
+                <Typography variant="h5" gutterBottom>ISBN: {book.isbn}</Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" color="primary" onClick={()=> {}}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
+
+    );
+}
+
+export default Book;
---
+++ client/src/components/Books/books.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import Book from './Book/book';
+// import useStyles from './styles';
+
+const Books = () => {
+    const books = useSelector((state) => state.books);
+    // const classes = useStyles();
+    console.log(books);
+    return(
+        !books.length ?  <CircularProgress /> : (
+            <Grid container alignItems="stretch" spacing={3}>
+                {books.map((book) => (
+                    <Grid key={book._id} item xs={12} sm={6}>
+                        <Book book={book} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    );
+}
+
+export default Books;
