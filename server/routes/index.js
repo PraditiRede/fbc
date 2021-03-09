e50dcba0ed14f3297d1737d8bb1c996dd64e9aaa
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-09 16:54:29 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,4 @@
 module.exports.auth = require("./auth");
+module.exports.friends = require("./friends");
+module.exports.profileUpdate = require("./profileUpdate");
+module.exports.inventory = require("./inventory");
