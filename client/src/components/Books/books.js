61849cefbcbe20002ab93a5152589622c258421d
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-09 17:08:36 +0530

    Update books.js

--- client/src/components/Books/books.js
+++ client/src/components/Books/books.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Book from './Book/book';
-// import useStyles from './styles';
+import useStyles from './styles';
 
 const Books = () => {
     const books = useSelector((state) => state.books);
-    // const classes = useStyles();
-    console.log(books);
+    const classes = useStyles();
+
+    // console.log(books);
     return(
         !books.length ?  <CircularProgress /> : (
-            <Grid container alignItems="stretch" spacing={3}>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {books.map((book) => (
                     <Grid key={book._id} item xs={12} sm={6}>
                         <Book book={book} />
