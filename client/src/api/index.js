86ac33f88981759b0848a15bc89fcd93afc9fc46
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-10 20:34:20 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 const url = "http://localhost:4000/books";
+const key = 'AIzaSyDPAr8qCQStR1K42vVM96XD9uiqjp9O4Kk';
 
 export const fetchBooks = () => axios.get(url);
 export const createBook = (newBook) => axios.post(url, newBook);
 
-const urla = "https://www.googleapis.com/books/v1/volumes?q={search terms}";
-export const getapiBooks = () => axios.get(urla);
+export default{
+    searchBooks: function(query){
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q="+query+"&key="+key);
+    },
+};
