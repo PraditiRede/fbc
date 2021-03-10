e7c8cb3c5c610ddd786a3f984801ba3d5c4c1ee9
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-10 20:23:44 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import App from './App';
 import reducers from'./reducers';
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
