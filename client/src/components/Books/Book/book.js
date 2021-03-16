eea9a7fe9c0b6adbba8b34206e5769b2c69cad2e
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-16 19:03:04 +0530

    Update book.js

--- client/src/components/Books/Book/book.js
+++ client/src/components/Books/Book/book.js
@@ -1,63 +1,41 @@
 import { useState } from 'react';
 import React from 'react';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'; 
-// import ThumbUpAltIcon from '@material-ui/icon/ThumbUpAl';
-import DeleteIcon from '@material-ui/icons/Delete';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import moment from 'moment';
-// import { post } from '../../../../../server/routes/auth';
+import { Card, Grid } from '@material-ui/core'; 
 import api from '../../../api/index';
-
+import Bookresult from './bookresult';
 import useStyles from './styles';
 
 const Book = ({ book }) => {
-
     const classes = useStyles();
     const query = book.search;
-
+    const isbn = 'isbn';
+    const ser = '';
     const [cards, setCards]= useState([]);
-    api.searchBooks(query)
-        .then(res =>
-            setCards(res.data.items)
-        )
-        .catch(err => console.log(err));
-        
-    var title = cards[0].volumeInfo.title;
-    var imge = cards[0].volumeInfo.imageLinks.thumbnail;
-    var publisher = cards[0].volumeInfo.publisher;
-    var author = cards[0].volumeInfo.authors[0];
-    var isbn = cards[0].industryIdentifiers[0].identifier;
-
-    // var title = book.booktitle;
-    // var imge = book.img;
-    // var publisher = book.publisher;
-    // var author = book.author;
-    // var isbn = book.isbn;
-
+    api.searchBooks(ser, isbn, query)
+    .then(res =>
+        setCards(res.data.items)
+    )
+    .catch(err => console.log(err));
     return(
-       <Card className={classes.card}>
-           <Typography variant="h5" className={classes.title}>Title: {title}</Typography>
-           <img src={imge} height="320" width="350"/>
-           <CardContent>
-               <Typography variant="h6">Posted By: {book.username}</Typography>
-               <Typography variant="body2">Posted {moment(book.createdAt).fromNow()}</Typography>
-           </CardContent>
-           {/* <div>
-                <Typography variant="body2" color="textSecondary">{book.tags.map((tag) => '#${tag} ')}</Typography>
-           </div> */}
-           <CardContent>
-                <Typography variant="h6" gutterBottom>Author: {author}</Typography>
-                <Typography variant="h6" gutterBottom>Publisher: {publisher}</Typography>
-                <Typography variant="h6" gutterBottom>ISBN: {isbn}</Typography>
-            </CardContent>
-            <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=> {}}>
-                    <DeleteIcon fontSize="small" />
-                    Delete
-                </Button>
-            </CardActions>
+        <Card >
+            <Grid >
+            { 
+                cards==undefined ? (
+                    <div>
+                        <div className={classes.invalid}>No book added yet...</div>
+                    </div>
+                ):(
+                    <Grid>
+                        {cards.map((cards) => (
+                            <Grid key={cards.id} >
+                                <Bookresult cards={cards} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                )
+            }
+            </Grid>
         </Card>
-
     );
 }
 
