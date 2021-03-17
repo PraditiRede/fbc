fccaa299a28301ed2e4629e91821bdec8a57990e
PraditiRede <52135720+PraditiRede@users.noreply.github.com>
2021-03-17 19:23:30 +0530

    Update bookresult.js

--- client/src/components/Books/Book/bookresult.js
+++ client/src/components/Books/Book/bookresult.js
@@ -5,30 +5,26 @@ const Bookresult = ( { cards }) => {
     const classes = useStyles();
 
     return (
-        <Card className={classes.card}>
-            <Grow in>
-                <Grid item >
-                    { 
-                        cards.volumeInfo.imageLinks==undefined ? (
-                            <div>
-                                <img src='' height="210" width="150" className={classes.image}/>
-                                <div className={classes.imagefail}>Thumbnail not available</div>
-                            </div>
-                        ):(
-                            <img src={cards.volumeInfo.imageLinks.thumbnail} height="210" width="150" className={classes.image} />
-                        )
-                    }
-                    {
-                        cards.volumeInfo.title==undefined ? (
-                            <div className={classes.title}>Title: Not available</div>
-                        ):(
-                            <div className={classes.title}>{cards.volumeInfo.title }</div>
-                        )
-                    }
-                    <Button className={classes.button}>Delete</Button>
-                </Grid >
-            </Grow>
-        </Card>
+        <div className={classes.card}>
+            { 
+                cards.volumeInfo.imageLinks==undefined ? (
+                    <div>
+                        <img src='' height="230" width="140" className={classes.image}/>
+                        <div className={classes.imagefail}>Thumbnail not available</div>
+                    </div>
+                ):(
+                    <img src={cards.volumeInfo.imageLinks.thumbnail} height="230" width="140"  className={classes.image} />
+                )
+            }
+            {
+                cards.volumeInfo.title==undefined ? (
+                    <div className={classes.title}>Title: Not available</div>
+                ):(
+                    <div className={classes.title}>{cards.volumeInfo.title }</div>
+                )
+            }
+            {/* <Button className={classes.button}>Delete</Button> */}
+        </div>
     )
 }
 export default Bookresult;
